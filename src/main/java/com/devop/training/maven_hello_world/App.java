a70756126784833f569afac7a9606a981ed98ddc
Yadav <swati.a.yadav@capgemini.com>
2020-01-31 08:59:58 +0530

    commit

--- src/main/java/com/devop/training/maven_hello_world/App.java
+++ src/main/java/com/devop/training/maven_hello_world/App.java
@@ -1,10 +1,15 @@
 package com.devop.training.maven_hello_world;
 
 /**
- * Hello world!
- *
+ * Hello world!.
  */
-public class App {
+public class App{
+    
+    /**
+     * The main method.
+     *
+     * @param args the arguments
+     */
     public static void main(final String[] args){
         System.out.println("Hello World!");
     }
